efe88916bbd01e3ded4e01ef1745146aead60cec
Latcho <latcho.nikolov@gmail.com>
2018-09-28 08:23:41 +0300

    restful services now log any request on the console

--- src/main/java/com/telerikacademy/shoecatalogueappspring/controllers/Products.java
+++ src/main/java/com/telerikacademy/shoecatalogueappspring/controllers/Products.java
@@ -5,8 +5,8 @@ import com.telerikacademy.shoecatalogueappspring.models.Product;
 import com.telerikacademy.shoecatalogueappspring.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -20,39 +20,52 @@ public class Products {
     }
 
     @GetMapping
-    public List<Product> getProducts() {
+    public List<Product> getProducts(HttpServletRequest request) {
+        Date date = new Date();
+        System.out.println("New Get Request: All products Requested at " + date + "; IP: " + request.getRemoteAddr());
         return service.getProducts();
     }
 
     @GetMapping("/colors")
-    public List<Colors> getProductColors() {
+    public List<Colors> getProductColors(HttpServletRequest request) {
+        Date date = new Date();
+        System.out.println("New Get Request: Colors Requested at " + date + "; IP: " + request.getRemoteAddr());
         return service.getProductColors();
     }
 
     @GetMapping("/sizes")
-    public List<Integer> getProductSizes() {
-       return service.getProductSize();
+    public List<Integer> getProductSizes(HttpServletRequest request) {
+        Date date = new Date();
+        System.out.println("New Get Request: Sizes Requested at " + date + "; IP: " + request.getRemoteAddr());
+        return service.getProductSize();
     }
 
     @PostMapping("/create")
-    public void createProduct(@RequestBody Product product) {
+    public void createProduct(@RequestBody Product product, HttpServletRequest request) {
+        Date date = new Date();
+        System.out.println("New Post Request: Create Product Requested at " + date + "; IP: " + request.getRemoteAddr());
         service.createProduct(product);
     }
 
     @GetMapping("/{id}")
-    public Product getProductById(@ PathVariable int id) {
-       return service.getProductById(id);
+    public Product getProductById(@ PathVariable int id, HttpServletRequest request) {
+        Date date = new Date();
+        System.out.println("New Get Request: Product with ID " + id + " Requested at " + date + "; IP: " + request.getRemoteAddr());
+        return service.getProductById(id);
     }
 
     @PutMapping("/{id}")
-    public void updateProduct (@PathVariable int id, @RequestBody Product product) {
+    public void updateProduct (@PathVariable int id, @RequestBody Product product, HttpServletRequest request) {
+        Date date = new Date();
+        System.out.println("New Put Request: Update on product with ID " + id + " Requested at " + date + "; IP: " + request.getRemoteAddr());
         service.updateProduct(id, product);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable int id) {
+    public void deleteProduct(@PathVariable int id, HttpServletRequest request) {
+        Date date = new Date();
+        System.out.println("New DELETE Request: Delete on product with ID + " + id + " Requested at " + date + "; IP: " + request.getRemoteAddr());
         service.deleteProduct(id);
     }
-
 
 }
